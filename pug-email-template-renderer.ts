4bd1b84e01d164b2dea39263d6e602c8af731e07
Hugo bessa <hugo@vinta.com.br>
2025-02-12 03:37:44 -0300

    Update to vintasend 0.1.1

--- pug-email-template-renderer.ts
+++ pug-email-template-renderer.ts
@@ -5,16 +5,20 @@ import type {
 } from 'vintasend/src/services/notification-template-renderers/base-email-template-renderer';
 import type { JsonObject } from 'vintasend/src/types/json-values';
 import type { Notification } from 'vintasend/src/types/notification';
+import type { Identifier } from 'vintasend/src/types/identifier';
 
 import pug from 'pug';
 
-export class PugEmailTemplateRenderer<AvailableContexts extends Record<string, ContextGenerator>>
-  implements BaseEmailTemplateRenderer<AvailableContexts>
+export class PugEmailTemplateRenderer<
+  AvailableContexts extends Record<string, ContextGenerator>,
+  NotificationIdType extends Identifier = Identifier,
+  UserIdType extends Identifier = Identifier,
+> implements BaseEmailTemplateRenderer<AvailableContexts, NotificationIdType, UserIdType>
 {
   constructor(private options: pug.Options = {}) {}
 
   render(
-    notification: Notification<AvailableContexts>,
+    notification: Notification<AvailableContexts, NotificationIdType, UserIdType>,
     context: JsonObject,
   ): Promise<EmailTemplate> {
     const bodyTemplate = pug.compileFile(notification.bodyTemplate, this.options);
