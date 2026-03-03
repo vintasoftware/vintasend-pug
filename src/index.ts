79d86e2efb02cbcd09bb4c3de218e845eb2499f0
Hugo bessa <hugo@vinta.com.br>
2026-03-03 04:35:06 -0300

    Release vintasend-pug@0.12.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { PugEmailTemplateRenderer } from './pug-email-template-renderer';
-export { PugEmailTemplateRendererFactory } from './pug-email-template-renderer';
-export type { PugInlineEmailTemplateRenderer } from './pug-inline-email-template-renderer';
-export { PugInlineEmailTemplateRendererFactory } from './pug-inline-email-template-renderer';
+export type { PugEmailTemplateRenderer } from './pug-email-template-renderer.js';
+export { PugEmailTemplateRendererFactory } from './pug-email-template-renderer.js';
+export type { PugInlineEmailTemplateRenderer } from './pug-inline-email-template-renderer.js';
+export { PugInlineEmailTemplateRendererFactory } from './pug-inline-email-template-renderer.js';
